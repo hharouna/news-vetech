b0a5a232e805b9378af950759aa87eb695ea94b9
hharouna86 <hharouna86@gmail.com>
2019-09-13 17:05:11 +0000

    comite

--- p_register/f_quartier.php
+++ p_register/f_quartier.php
@@ -9,12 +9,12 @@ class f_af{
 		$sql_quartier = 'SELECT *FROM '.$__af_db['annonce'].'.region_quartier	 WHERE '.$__af_db['annonce'].'.region_quartier.id_re_quart=? '; 
 		$q= $_db->prepare($sql_quartier);
 		$q->execute(array($id__region));
-		//$count_liste = $q->rowCount();
+		$count_liste = $q->rowCount();
 		$result_quart= $q->fetchAll(PDO::FETCH_ASSOC);  
-	    $count_liste = "1"; 
-		$f_quatier="<option >-- Quartier --</option>";
+	 
+	    $f_quatier="<option >-- Quartier --</option>";
 		foreach($result_quart as $rs_quart => $_qua){
-		$f_quatier.= "<option  id='' >".$_qua['com']."</option>"; 
+		$f_quatier.= "<option  id='".$this->base64encode($_qua['id_quarier'])."' >".$_qua['nom_quartier']."</option>"; 
 		}
 		$f_quatier.=""; 
 		$table = $count_liste."/--/".$f_quatier; // return liste des regions
